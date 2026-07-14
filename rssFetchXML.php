3460714dc9e77f3a197ecfea2e1165b23cea878b
Mike Jordan <mtjo62@gmail.com>
2026-07-14 17:47:13 -0400

    Refactor rssFetchXML class and update version
    Refactored rssFetchXML class to RssFetchXml with strict types and improved caching logic. Added method to use file_get_contents and 
    stream_context_create if allow_url_fopen wrapper is enabled. Updated to PSR-12  standards

--- rssFetchXML.php
+++ rssFetchXML.php
@@ -1,113 +1,152 @@
-<?php
+<?php declare(strict_types=1);
 
 /**
  * RSS aggregator utilizing libxml and cURL
  *
- * @package rssFetchXML
- * @version 1.1
+ * @package RssFetchXml
+ * @version 1.2
  * @author MT Jordan <mtjo62@gmail.com>
- * @copyright 2025
+ * @copyright 2026
  * @license MIT
  */
 
-class rssFetchXML
+class RssFetchXml
 {
     /**********************************************
      * Private Class Properties
      *********************************************/
 
-    private $rss_cache_xml = false;
-    private $rss_cache_file;
-    private $rss_fetch_url;
-    private $rss_str_xml = false;
+    /**
+     * @var string
+     */
+    private string $xml_cache_dir;
+    
+    /**
+     * @var int
+     */
+    private int $xml_cache_expire;
 
-    /**********************************************
-     * Public Class Properties
-     *********************************************/
-
-    public $rss_xml_cache = ""; //Must be set
-    public $rss_xml_expire = 864e2;
- 
     /**********************************************
      * Public methods
      *********************************************/
 
-    public function __construct() {}
+    /**
+     * Constructor
+     *
+     * @param string $cache_dir
+     * @param int $expire_time
+     */
+    public function __construct(string $cache_dir="./", int $expire_time=3600) {
+        $this->xml_cache_dir = $cache_dir;
+        $this->xml_cache_expire = $expire_time;
+    }
 
-    /* Set RSS URL and cache file name
+    /**
+     * Set RSS URL and cache file name
      * Load unexpired XML file or fetch
      * RSS feed and return XML object
+     *
+     * @param string $rss_url
+     * @return mixed
      */
-    public function rss_xml_fetch($rss_url) {
-        //set private properties
-        $this->rss_fetch_url = $rss_url;
-        $this->rss_cache_file =
-            $this->rss_xml_cache .
-            str_replace(
-                " ",
-                "",
-                preg_replace("/[^A-Za-z0-9 ]/", "", $rss_url) . ".xml"
-            );
+    public function rssXmlFetch(string $rss_url) {
+        $xml_cache_file = $this->xml_cache_dir . str_replace(" ", "", preg_replace("/[^A-Za-z0-9 ]/", "", $rss_url) . ".xml");
+        $xml_cache_obj = $this->rssXmlCache($xml_cache_file);
 
-        if ($this->rss_xml_cache()) {
-            return $this->rss_cache_xml;
+        if ($xml_cache_obj !== false) {
+            return $xml_cache_obj;
         }
 
-        if ($this->rss_xml_str()) {
-            return $this->rss_str_xml;
-        } else {
-            return false;
+        $xml_str_obj = $this->rssXmlStr($rss_url, $xml_cache_file);
+
+        if ($xml_str_obj !== false) {
+            return $xml_str_obj;
         }
-    }
+
+        return false;
+     }
 
     /**********************************************
      * Private methods
      *********************************************/
 
-    /* Load unexpired XML cache file */
-    private function rss_xml_cache()
-    {
-        if (
-            file_exists($this->rss_cache_file) &&
-            time() - filemtime($this->rss_cache_file) < $this->rss_xml_expire
-        ) {
-            $this->rss_cache_xml = simplexml_load_file($this->rss_cache_file);
+    /**
+     * Return unexpired XML cache object
+     *
+     * @param string $cache_file
+     * @return mixed
+     */
+    private function rssXmlCache(string $cache_file) {
+        if (file_exists($cache_file) && time() - filemtime($cache_file) < $this->xml_cache_expire) {
+            return simplexml_load_file($cache_file, 'SimpleXMLElement', LIBXML_NOWARNING | LIBXML_NOERROR);
         }
 
-        if (is_object($this->rss_cache_xml)) {
-            return true;
-        }
+        return false;
     }
 
-    /* Fetch RSS XML, cache XML file and return XML object */
-    private function rss_xml_str()
-    {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $this->rss_fetch_url);
+    /**
+     * Return XML object
+     *
+     * @param string $rss_url
+     * @param string $cache_file
+     * @return mixed
+     */
+    private function rssXmlStr(string $rss_url, string $cache_file) {
+        if (ini_get("allow_url_fopen")) {
+            return $this->rssXmlStream($rss_url, $cache_file);
+        } else {
+            return $this->rssXmlCurl($rss_url, $cache_file);
+        }
+    }
+    
+    /**
+     * Fetch RSS XML via cURL
+     *
+     * @param string $rss_url
+     * @param string $cache_file
+     * @return mixed
+     */
+    private function rssXmlCurl(string $rss_url, string $cache_file) {
+        $curl = curl_init($rss_url);
         curl_setopt($curl, CURLOPT_HEADER, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_USERAGENT, @$_SERVER["HTTP_USER_AGENT"]);
+        curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.3");
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($curl, CURLOPT_ENCODING, "utf-8");
         $curl_result = curl_exec($curl);
-        $curl_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        
-        $this->rss_str_xml = simplexml_load_string($curl_result);
-        
-        curl_close($curl);
-        
-        //Sanity check for valid simplexml object - helps prevent fatal errors
-        //triggered by asXML by invalid RSS URLs - not 100% accurate
-        if (!is_object($this->rss_str_xml) || $curl_code !== 200) {
+        $curl_str_obj = simplexml_load_string((string)$curl_result, 'SimpleXMLElement', LIBXML_NOWARNING | LIBXML_NOERROR);
+
+        //Sanity check for simplexml object, $curl_result and HTTP_CODE
+        //Prevents fatal errors triggered by asXML - not 100% accurate
+        if (!is_string($curl_result) || $curl_str_obj === false || curl_getinfo($curl, CURLINFO_HTTP_CODE) !== 200) {
             return false;
         }
-            
-        if ($this->rss_str_xml->asXML($this->rss_cache_file)) {
-            return true;
+        
+        if ($curl_str_obj->asXML($cache_file) === true) {
+            return $curl_str_obj;
         }
+
+        return false;
+    }
+    
+    /**
+     * Fetch RSS XML via stream_context_create & file_get_contents
+     *
+     * @param string $rss_url
+     * @param string $cache_file
+     * @return mixed
+     */
+    private function rssXmlStream(string $rss_url, string $cache_file) {
+        $stream_context = stream_context_create( ["http" => ["user_agent" => "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36"]]);
+        $stream_response = file_get_contents($rss_url, false, $stream_context);
+        $stream_str_obj = simplexml_load_string((string)$stream_response, "SimpleXMLElement", LIBXML_NOWARNING | LIBXML_NOERROR);  
+      
+        if (is_string($stream_response) && $stream_str_obj !== false && $stream_str_obj->asXML($cache_file) === true) {
+            return $stream_str_obj;
+        }
+
+        return false;
     }
 }
-/* EOF rssFetchXML.php */
-/* Location: ./rssFetchXML.php */
-
-
+/* EOF rssFetchXml.php */
+/* Location: ./rssFetchXml.php */
