e8f9e98f32b53fa65d483541558e76e1e21ed7f1
Mike Jordan <mtjo62@gmail.com>
2026-07-22 12:13:40 -0400

    Add files via upload

---
+++ rss_config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Path to cache dir
+$rss_cache = "path\to\cache\\";
+//Cache expire in secs - ie: 3600 = 1 hr
+$rss_expire = 3600;
+//Custom error message on feed failure
+$rss_error = "RSS fetch failed - RSS url or feed may be unavailable";
+// Max # of links to display
+$max_links = 12;
